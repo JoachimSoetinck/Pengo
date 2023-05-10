6769ddf7bd4f59ef3f42dcc73f03d1effa411f19
Joachim Soetinck <joachim.soetinck99@gmail.com>
2023-05-10 17:54:27 +0200

    Update main

---
+++ Pengo/Main.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void CreatePlayer(std::shared_ptr<dae::Font>& font, std::shared_ptr<dae::Scene>& scene, std::shared_ptr<dae::GameObject>& go);
+
+void CreateInfo(std::shared_ptr<dae::Font>& font, std::shared_ptr<dae::Scene>& scene, glm::ivec2& pos2, glm::ivec2& pos);
