2f1b24fee045e72b8eb23ee9ce9a9d0dbd1fe232
Joachim Soetinck <joachim.soetinck99@gmail.com>
2023-05-10 14:23:46 +0200

    removing include

--- Minigin/ServiceLocator.cpp
+++ Minigin/ServiceLocator.cpp
@@ -1,6 +1,5 @@
 #include "MiniginPCH.h"
 #include "ServiceLocator.h"
-#include "SoundSystem.h"
 
 std::unique_ptr<dae::NullSoundSystem> dae::ServiceLocator::ss_nullService{};
 std::unique_ptr<dae::ISoundSystem> dae::ServiceLocator::ss_service{ std::move(ss_nullService) };
